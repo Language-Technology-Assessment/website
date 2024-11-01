5f7be369487be375fe254355e4ddc96bb621dad0
BSTN <basten@bstn.nl>
2024-11-01 14:11:28 +0000

    update to custom image

---
+++ components/content/prose-img.vue
@@ -0,0 +1,17 @@
+<template>
+  <NuxtImg class="inline-image" :src="src" sizes="1200px sm:500px md:800px"></NuxtImg>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(['src'])
+const src = computed(() => {
+  return `https://raw.githubusercontent.com/Language-Technology-Assessment/European-open-AI-index/main${props.src}`
+})
+</script>
+
+<style lang="less" scoped>
+.inline-image {
+  display: block;
+  margin: 1rem auto 2rem;
+}
+</style>
