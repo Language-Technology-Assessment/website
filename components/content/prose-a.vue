7b16681c15abc9854be5a5221e578b2327129562
BSTN <basten@bstn.nl>
2024-11-07 10:27:23 +0000

    _blank + blockquote

---
+++ components/content/prose-a.vue
@@ -0,0 +1,26 @@
+<template>
+  <NuxtLink :href="props.href" :target="customTarget">
+    <slot />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { PropType } from 'vue'
+
+const customTarget = computed(() => {
+  if (props.target) return props.target
+  if (props.href.startsWith('http')) return '_blank'
+})
+
+const props = defineProps({
+  href: {
+    type: String,
+    default: ''
+  },
+  target: {
+    type: String as PropType<'_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined>,
+    default: undefined,
+    required: false
+  }
+})
+</script>
