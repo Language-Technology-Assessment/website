0311f398776a78c5607ef43cb88b1e266b20d699
BSTN <basten@bstn.nl>
2025-01-06 11:39:02 +0000

    add author and person component, fix h3-h6

---
+++ components/content/author.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="author">by {{ props.author }}</div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["author"]);
+</script>
+
+<style lang="less" scoped>
+.author {
+  text-align: center;
+  margin-top: -2rem;
+  margin-bottom: 4rem;
+  font-size: 0.8rem;
+  color: var(--fg2);
+}
+</style>
---
+++ components/content/person.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="person">
+    <div class="head">
+      <div class="image">
+        <NuxtImg :src="props.image" :alt="props.name"></NuxtImg>
+      </div>
+      <div class="name">
+        {{ props.name }}
+      </div>
+    </div>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["image", "name"]);
+</script>
+
+<style lang="less" scoped>
+.person {
+  background: var(--bg2);
+  padding: 1.5rem 2rem;
+  border-radius: 0.5rem;
+  width: var(--pwidth);
+  max-width: var(--maxwidth);
+  margin: 2rem auto 2rem;
+  .head {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 1rem;
+    margin-bottom: 1.5rem;
+    .image {
+      width: 4rem;
+      height: 4rem;
+      flex-shrink: 0;
+      img {
+        border-radius: 100%;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+    .name {
+      font-size: 1.125rem;
+      font-weight: 600;
+      color: var(--fg);
+    }
+  }
+  .content {
+    :deep(p) {
+      width: 100%;
+      max-width: 100%;
+      margin: 0 auto 1rem;
+      font-weight: 400;
+      font-size: 0.8rem;
+      line-height: 1.6;
+    }
+  }
+}
+</style>
