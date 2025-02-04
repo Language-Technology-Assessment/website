0da50c65298d9ecb932dd1d7d1ff475f805a3ca1
BSTN <basten@bstn.nl>
2025-02-04 13:42:48 +0000

    fix image

--- components/content/prose-img.vue
+++ components/content/prose-img.vue
@@ -1,12 +1,19 @@
 <template>
-  <NuxtImg class="inline-image" :src="src" sizes="1200px sm:500px md:800px"></NuxtImg>
+  <NuxtImg
+    class="inline-image"
+    :src="src"
+    sizes="1200px sm:500px md:800px"
+  ></NuxtImg>
 </template>
 
 <script lang="ts" setup>
-const props = defineProps(['src'])
+const props = defineProps(["src"]);
 const src = computed(() => {
-  return `https://raw.githubusercontent.com/Language-Technology-Assessment/European-open-AI-index/main${props.src}`
-})
+  return `https://raw.githubusercontent.com/Language-Technology-Assessment/European-open-AI-index/main${props.src}`;
+});
+defineOptions({
+  inheritAttrs: false,
+});
 </script>
 
 <style lang="less" scoped>
