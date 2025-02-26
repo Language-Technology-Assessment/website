dd2c838a0854be412c4819b4ef9dcec56a2b6494
BSTN <basten@bstn.nl>
2025-02-26 10:24:54 +0000

    Adding date component

---
+++ components/content/date.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="date">
+    <slot mdc-unwrap="p">{{ props.date }}</slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps(["date"]);
+</script>
+
+<style lang="less" scoped>
+.date {
+  text-align: center;
+  margin-top: -2rem;
+  margin-bottom: 4rem;
+  font-size: 0.8rem;
+  color: var(--fg2);
+}
+</style>
