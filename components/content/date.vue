27de496980349d48c824a929964a4c951e6965cf
BSTN <basten@bstn.nl>
2025-02-27 11:01:40 +0000

    update date component

--- components/content/date.vue
+++ components/content/date.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="date">
-    <slot mdc-unwrap="p">{{ props.date }}</slot>
+    <slot mdc-unwrap="p">{{ useDateFormat(props.date, "DD MMMM YYYY") }}</slot>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useDateFormat } from "@vueuse/core";
 const props = defineProps(["date"]);
 </script>
 
 <style lang="less" scoped>
 .date {
   text-align: center;
-  margin-top: -2rem;
+  margin-top: -3rem;
   margin-bottom: 4rem;
-  font-size: 0.8rem;
+  font-size: 0.7rem;
   color: var(--fg2);
 }
 </style>
