0d7ddfa4c0599c39fd02299eb87e5f949ae3cce8
BSTN <basten@bstn.nl>
2026-01-07 11:09:56 +0000

    fix landing

--- components/content/landing.vue
+++ components/content/landing.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class="landing mx-auto mt-0 mb-24 max-h-none max-w-container-max overflow-visible transition-opacity duration-1000 lg:h-auto portrait:max-h-[46rem]"
+      class="landing mx-auto mt-0 mb-24 max-h-none max-w-container-max overflow-visible transition-opacity duration-1000 lg:h-74! portrait:max-h-[46rem]"
       :class="{ 'opacity-0': !isVisible, 'opacity-100': isVisible }"
       ref="mainelement"
     >
